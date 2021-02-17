86b7dca8f17fc4a34df19a9f7ecc3b261f956b12
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-02-17 17:41:38 +0200

    Atoi created.

--- seminar1/problema2/problema2.cpp
+++ seminar1/problema2/problema2.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-//sum
-
 //atoi
+int a_to_i(char s[])
+{
+    int nr = 0;
+    for(int i=0;i<strlen(s);i++)
+    {
+        if(s[i]>='0' && s[i]<='9')
+        {
+            nr = nr*10+(s[i]-'0');
+        }
+    }
+    nr = s[0]=='-' ? -nr : nr;
+    cout << s << ' ' << nr  << endl;
+    return nr;
+}
 
 int main()
 {
     //read file
     FILE * file;
     file = fopen ("ini.txt","r");
+    int sum = 0;
     while(!feof(file))
     {
         char line[255] = "";
         fgets(line,sizeof(line), file);
-        cout << line << endl;
+        sum += a_to_i(line);
     }
 }
