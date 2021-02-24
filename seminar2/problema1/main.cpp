425d80f52211b1e82a2c262a30c270aba706d0bf
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-02-24 17:02:51 +0200

    Problema 1: Testare

--- seminar2/problema1/main.cpp
+++ seminar2/problema1/main.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main()
 {
+    NumberList nr;
+    nr.Init();
+    nr.Add(20);
+    nr.Add(15);
+    nr.Add(30);
+    nr.Print();
+    nr.Sort();
+    nr.Print();
     return 0;
 }
