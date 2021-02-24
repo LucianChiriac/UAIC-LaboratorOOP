9b1e85e2b1a4d4ec5d22a55a860f8f8bb903df61
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-02-24 17:04:43 +0200

    Problema 2: Boilerplate

---
+++ seminar2/problema2/Students.h
@@ -0,0 +1,17 @@
+class Student
+{
+    char *name;
+    float mathematics;
+    float english;
+    float history;
+public:
+    char* get_name();
+    void set_name(char* name);
+    float& get_mathematics();
+    void set_mathematics(float x);
+    float& get_english();
+    void set_english(float x);
+    float& get_history();
+    void set_history(float x);
+    float Average();
+};
---
+++ seminar2/problema2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "functions.h"
+#include "Students.h"
+
+int main()
+{
+    
+}
