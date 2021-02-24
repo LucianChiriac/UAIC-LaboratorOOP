61e72bfd575af9366fd7794b78b9ba950e0c0a52
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-02-24 17:00:02 +0200

    Problema 1: Sortare si print

--- seminar2/problema1/NumberList.cpp
+++ seminar2/problema1/NumberList.cpp
@@ -17,3 +17,23 @@ bool NumberList::Add(int x)
     return true;
     
 }
+void NumberList::Sort()
+{
+    bool sortat;
+    do{
+        sortat=true;
+        for(int i=0;i<this->count-1;i++)
+            for(int j=i+1;j<this->count;j++)
+                if(this->numbers[i]>this->numbers[j])
+                {
+                    sortat=false;
+                    swap(this->numbers[i], this->numbers[j]);
+                }
+    }while(!sortat);
+}
+void NumberList::Print()
+{
+    for(int i=0;i<this->count;i++)
+        cout << this->numbers[i] << ' ';
+    cout << '\n';
+}
