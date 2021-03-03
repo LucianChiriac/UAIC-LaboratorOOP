830f044696ce3723f1903897325c1ff47e4c6648
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-03-03 16:09:51 +0200

    Problema 1: Add functions

--- seminar3/problema1/Math.cpp
+++ seminar3/problema1/Math.cpp
@@ -1,2 +1,19 @@
 #include "Math.h"
 
+int Math::Add(int a,int b)
+{
+    return (a+b);
+}
+int Math::Add(int a,int b,int c)
+{
+    return (a+b+c);
+}
+
+int Math::Add(double a,double b)
+{
+    return (int)(a+b);
+}
+int Math::Add(double a,double b,double c)
+{
+    return (int)(a+b+c);
+}
