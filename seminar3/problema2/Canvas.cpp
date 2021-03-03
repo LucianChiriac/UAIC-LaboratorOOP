1e174d1c55853e52972f27b53af03e7f189dc4c5
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-03-03 17:15:44 +0200

    Problema 2: Set point, part of drawline

--- seminar3/problema2/Canvas.cpp
+++ seminar3/problema2/Canvas.cpp
@@ -7,13 +7,14 @@ Canvas::Canvas(int width,int height)
     this->matrix = new char*[height];
     for(int i=0;i<height;i++)
         this->matrix[i] = new char[width];
+    this->Clear();
 }
 void Canvas::Print()
 {
     for(int i=0;i<this->height;i++)
     {
         for(int j=0;j<this->width;j++)
-            std::cout << this->matrix[i] << ' ';
+            std::cout << this->matrix[i][j] << ' ';
         std::cout << '\n';
     }
 }
@@ -22,8 +23,32 @@ void Canvas::Clear()
 {
     for(int i=0;i<this->height;i++)
     {
-        delete this->matrix[i];
-        this->matrix[i] = new char[width];
+        for(int j=0;j<this->width;j++)
+            this->matrix[i][j]=' ';
     }
 }
 
+void Canvas::DrawLine(int x1, int y1, int x2, int y2, char ch)
+{
+    int dx = x2-x1;
+    int dy = y2-y1;
+    int D = 2*dy-dx;
+    int y=y1;
+    for(int x=x1;x<x2;x++)
+    {
+        this->matrix[x][y] = ch;
+        if(D>0)
+        {
+            y++;
+            D=D-2*dx;
+        }
+        D=D+2*dy;
+    }
+
+
+}
+
+void Canvas::SetPoint(int x,int y, char ch)
+{
+    this->matrix[x][y] = ch;
+}
--- seminar3/problema2/main.cpp
+++ seminar3/problema2/main.cpp
@@ -6,9 +6,11 @@ int main()
 {
     Canvas c = {10,20};
     
-    c.DrawLine();
+    // c.DrawLine();
+    c.SetPoint(4,5, 'x');
+    c.Clear();
+    c.DrawLine(1,1,3,3,'y');
     c.Print();
-    
     
     // c.Clear();
     return 0;
