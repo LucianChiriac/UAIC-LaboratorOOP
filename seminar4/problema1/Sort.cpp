18a43bc7eac5e76b84b8aa18d169b675d295e99a
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-03-10 16:19:12 +0200

    Problema 1: Boilerplate

---
+++ seminar4/problema1/Sort.cpp
@@ -0,0 +1,20 @@
+#include "Sort.h"
+
+void Sort::InsertSort(bool ascendent) {
+    
+}
+void Sort::QuickSort(bool ascendent) {
+
+}
+void Sort::BubbleSort(bool ascendent) {
+
+}
+void Sort::Print() {
+
+}
+int Sort::GetElementsCount(){
+    return 0;
+}
+int Sort::GetElementFromIndex(int index){
+    return 0;
+}
