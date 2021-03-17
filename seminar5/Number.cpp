0017bd54fa645fe0551e98cb8d4290420ec546ca
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-03-17 16:08:41 +0200

    Boilerplate done

---
+++ seminar5/Number.cpp
@@ -0,0 +1,29 @@
+#include "Number.h"
+using namespace std;
+
+Number::Number(const char * value, int base)
+{
+
+}
+
+Number::~Number()
+{
+
+}
+
+void Number::SwitchBase(int newBase)
+{
+
+}
+void Number::Print()
+{
+
+}
+int  Number::GetDigitsCount()
+{
+
+}
+int  Number::GetBase()
+{
+    
+}
