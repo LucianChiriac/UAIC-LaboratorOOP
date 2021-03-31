cc81f03944ea8ab14a0c54706333c0b9734ff7db
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-03-31 16:16:33 +0300

    Problema 1: Finished.

---
+++ seminar7/problema1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+float operator"" _Kelvin(unsigned long long int x)
+{
+    return x+273.15;
+}
+
+float operator"" _Fahrenheit(unsigned long long int x)
+{
+    return ((x*1.8)+32);
+}
+
+int main() {
+    float a = 300_Kelvin;
+    float b = 120_Fahrenheit;
+    cout << '\n';
+    return 0;
+}
