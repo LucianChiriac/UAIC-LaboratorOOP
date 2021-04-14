f3f8bd0d7148e2b64c3bc20ec0825e91459d94d1
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-04-14 16:41:06 +0300

    Seminar 8: word splitter finished

--- seminar8/main.cpp
+++ seminar8/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+#include <algorithm>
 using namespace std;
 ifstream f("file.in");
 
-void splitSentaceIntoWords(string str)
+void splitSentaceIntoWords(string str, map<string, int>& words)
 {
     const string separators=" ,?!.";
     size_t found = str.find_first_of(separators);
@@ -12,22 +14,33 @@ void splitSentaceIntoWords(string str)
     {
         string word = str.substr(0, found); 
         if(word.length())
-            cout << "WORD: " << word << " with length: " << word.length() << endl;
+        {
+            transform(word.begin(), word.end(), word.begin(), ::tolower);
+            if(words[word]!=0)
+            {
+                words[word]++;
+            }else{
+                words[word]=1;
+            }
+        }
         str.erase(0, found+1);
         found = str.find_first_of(separators);
     }
-    // cout << sentace << endl;
 }
 
 
 int main()
 {
+    map<string, int> words;
     string sentace;
     if(f.is_open())
         getline(f, sentace);
     f.close();
-    splitSentaceIntoWords(sentace);
-
+    splitSentaceIntoWords(sentace, words);
+    for(auto elem : words)
+    {
+        std::cout << elem.first << " " << elem.second << "\n";
+    }
     //linux debug
     cout << '\n';
     return 0;
