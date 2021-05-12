ec1d87707363a7a2b49822a9fe41d9f0d552e151
zaBogdan <bogdanzavadovschi17@gmail.com>
2021-05-12 16:10:01 +0300

    Add elements, Foreach, Insert

--- seminar11/array.h
+++ seminar11/array.h
@@ -1,4 +1,5 @@
 #include <exception>
+#include <iostream>
 using namespace std;
 
 class OutOfBounds : public exception
@@ -20,17 +21,50 @@ class ArrayIterator
 {
 private:
 	int Current; // mai adaugati si alte date si functii necesare pentru iterator
-
+	T* val;
 public:
-	ArrayIterator();
+	ArrayIterator(T* val)
+	{
+		this->val = val;
+		this->Current=0;
+	}
 
-	ArrayIterator& operator ++ ();
-	ArrayIterator& operator -- ();
+	ArrayIterator& operator ++ ()
+	{
+		this->val+=sizeof(T*);
+		this->Current++;
+		return *this;
+	}
+	ArrayIterator& operator -- ()
+	{
+		try{
+			if(this->Current<=0)
+				throw *(new OutOfBounds);
+			this->val--;
+			this->Current--;
+		}catch(exception& e){
+			std::cout << "[Error] " << e.what() << '\n';
+		}
+	}
 
-	bool operator= (ArrayIterator<T> &);
-	bool operator!=(ArrayIterator<T> &);
+	bool operator==(ArrayIterator<T> &tmp)
+	{
+		return this->val==tmp.val;
+	}
+	bool operator!=(ArrayIterator<T> &tmp)
+	{
+		return !(this->val==tmp.val);
+	}
 
-	T* GetElement();
+	T* GetElement()
+	{
+		return this->val;
+	}
+	T* operator*()
+	{
+		return this->GetElement();
+	}
+
 
 };
 
@@ -58,7 +92,7 @@ public:
         this->Capacity=capacity;
         this->List = new T*[this->Capacity];
         for(int i=0;i<this->Capacity;i++)
-            this->List[i]= new int;
+            this->List[i]= new T;
         this->Size = 0;
     }
     Array(const Array<T> &otherArray); // constructor de copiere
@@ -70,25 +104,63 @@ public:
         return this->List[index];
     }
 
-	const Array<T>& operator+=(const T &newElem) // adauga un element de tipul T la sfarsitul listei si returneaza this
+	// adauga un element de tipul T la sfarsitul listei si returneaza this
+	const Array<T>& operator+=(const T &newElem) 
     {
         if(this->Size>this->Capacity)
         {
-            
+			this->Capacity*=2;
+			T** tmp = (T**)realloc(this->List, this->Capacity*sizeof(T*));
+			this->List = tmp;
         }
         *this->List[this->Size++]= newElem;
         return *this;
     }
 
-	const Array<T>& Insert(int index, const T &newElem) // adauga un element pe pozitia index, retureaza this. Daca index e invalid arunca o exceptie
+	// adauga un element pe pozitia index, retureaza this. Daca index e invalid arunca o exceptie
+	const Array<T>& Insert(int index, const T &newElem) 
+	{
+		try{
+			if(index>this->Size || index<0 || index>this->Capacity)
+				throw *(new OutOfBounds);
+			*List[index]=newElem;
+		}catch(exception& e)
+		{
+			std::cout << "[Error] " << e.what() << '\n'; 
+		}
+		return *this;
+    }
+	// adauga o lista pe pozitia index, retureaza this. Daca index e invalid arunca o exceptie
+    const Array<T>& Insert(int index, const Array<T> otherArray)
 	{
 
-    }
-    const Array<T>& Insert(int index, const Array<T> otherArray); // adauga o lista pe pozitia index, retureaza this. Daca index e invalid arunca o exceptie
-	const Array<T>& Delete(int index); // sterge un element de pe pozitia index, returneaza this. Daca index e invalid arunca o exceptie
+	} 
+	
+	// sterge un element de pe pozitia index, returneaza this. Daca index e invalid arunca o exceptie
+	const Array<T>& Delete(int index)
+	{
+		try{
+			if(index>this->Size || index<0)
+				throw *(new OutOfBounds);
+			for(int i=index;i<this->Size-1;i++)
+			{
+				this->List[i] = this->List[i+1];
+			}
+			this->List[this->Size]=0;
+			this->Size--;
+		}catch(exception& e)
+		{
+			std::cout << "[Error] " << e.what() << '\n'; 
+		}
+		return *this;
+		
+	}
 
 
-	bool operator=(const Array<T> &otherArray);
+	bool operator=(const Array<T> &otherArray)
+	{
+
+	}
 
 
 	void Sort(); // sorteaza folosind comparatia intre elementele din T
@@ -104,7 +176,12 @@ public:
 	int Find(const T& elem, int(*compare)(const T&, const T&));//  cauta un element folosind o functie de comparatie
 	int Find(const T& elem, Compare *comparator);//  cauta un element folosind un comparator
 
-
+	void Print()
+	{
+		for(int i=0;i<this->GetSize();i++)
+			std::cout << *this->List[i] << ' ';
+		std::cout << '\n';
+	}
 	int GetSize()
     {
         return this->Size;
@@ -114,7 +191,24 @@ public:
         return this->Capacity;
     }
 
-	ArrayIterator<T> GetBeginIterator();
-	ArrayIterator<T> GetEndIterator();
+	ArrayIterator<T> GetBeginIterator()
+	{
+		ArrayIterator<T> tmp = {this->List[0]};
+		return tmp;
+	}
+	ArrayIterator<T> GetEndIterator()
+	{
+		ArrayIterator<T> tmp = {this->List[this->Size]};
+		return tmp;
+	}
+
+	ArrayIterator<T> begin()
+    {
+		return this->GetBeginIterator();
+    }
+    ArrayIterator<T> end()
+    {
+		return this->GetEndIterator();
+    }
 
 };
--- seminar11/main.cpp
+++ seminar11/main.cpp
@@ -5,13 +5,32 @@ using namespace std;
 int main()
 {
     Array<int> v = {100};
+    //get the capacity
     cout << v.GetCapacity() << '\n';
+    //add elements to the list
     v+=100;
     v+=214;
     v+=155;
     v+=423;
     v+=389;
+    //add variables to the list
+    int x=435;
+    v+=x;
+    //check the list
+    v.Print();
+    cout << '\n';
 
+    //insert at specific location
+    v.Insert(223,485);
+    v.Print();
+    //delete an element
+    v.Delete(6);
+    v.Print();
+    //for-each loop
+    for(auto key: v)
+    {
+        cout << *key  <<' ';
+    }
     cout << '\n';
     return 0;
 }
